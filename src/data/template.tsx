0a6d71f10879fb34b9b38f0feee8335749c0d6b8
Adnan Karim <adnan.karim.dev@gmail.com>
2024-11-18 14:17:35 -0700

    template for data filling

---
+++ src/data/template.tsx
@@ -0,0 +1,114 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "",
+  initials: "",
+  url: "",
+  location: "",
+  locationLink: "",
+  description: "",
+  summary: "",
+  avatarUrl: "",
+  skills: [],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" }
+  ],
+  contact: {
+    email: "",
+    tel: "",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "",
+        icon: Icons.github,
+        navbar: true
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "",
+        icon: Icons.linkedin,
+        navbar: true
+      },
+      X: {
+        name: "X",
+        url: "",
+        icon: Icons.x,
+        navbar: true
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "",
+        icon: Icons.youtube,
+        navbar: true
+      },
+      email: {
+        name: "Send Email",
+        url: "",
+        icon: Icons.email,
+        navbar: false
+      }
+    }
+  },
+  work: [
+    {
+      company: "",
+      href: "",
+      badges: [],
+      location: "",
+      title: "",
+      logoUrl: "",
+      start: "",
+      end: "",
+      description: ""
+    }
+  ],
+  education: [
+    {
+      school: "",
+      href: "",
+      degree: "",
+      logoUrl: "",
+      start: "",
+      end: ""
+    }
+  ],
+  projects: [
+    {
+      title: "",
+      href: "",
+      dates: "",
+      active: false,
+      description: "",
+      technologies: [],
+      links: [
+        {
+          type: "",
+          href: "",
+          icon: null
+        }
+      ],
+      image: "",
+      video: ""
+    }
+  ],
+  hackathons: [
+    {
+      title: "",
+      dates: "",
+      location: "",
+      description: "",
+      image: "",
+      mlh: "",  // Optional
+      win: "",   // Optional
+      links: [
+        {
+          title: "",
+          icon: null,
+          href: ""
+        }
+      ]
+    }
+  ]
+} as const;
